5523c67655ad8496d6d7cb65d225f907eb5e4ad9
Billy the Hustler <44713800+BillyTheMotherBoard@users.noreply.github.com>
2018-12-12 21:49:08 +0100

    Create aliases.zsh

---
+++ zsh/aliases.zsh
@@ -0,0 +1,18 @@
+# scripts
+alias taco="curl -L http://git.io/taco"
+alias pizza="curl -L http://git.io/pizzza"
+alias pizzza="curl -L http://git.io/pizzzza"
+alias rice="curl -L http://git.io/rice"
+alias poptart="curl -L http://git.io/poptart"
+alias waffles="curl -L http://git.io/waffles"
+alias pancakes="curl -L http://git.io/pancakes"
+alias burger="curl -L http://git.io/burger"
+alias vburger="curl -L http://git.io/vburger"
+alias pipes="pipes.sh"
+
+# system commands
+alias zshrc="vim $HOME/.zshrc"
+alias vimrc="vim $HOME/.vimrc"
+alias update="yay -Syu --combinedupgrade --save"
+alias rg="ranger"
+alias c="clear"
